5f70bceb95cc47ebde1ad62104ddce21335a6f64
Dawgora <dawgora@protonmail.com>
2025-06-06 13:26:59 +0300

    format

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -7,9 +7,9 @@
 let
   #unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in
-{
-  imports =
-    [ # Include the results of the hardware scan.
+  {
+    imports =
+      [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./modules/nginx/nginx.nix
       ./modules/zsh/zsh.nix
@@ -17,37 +17,37 @@ in
     ];
 
     home-manager = {
-        extraSpecialArgs = {inherit inputs; };
-        users = {
-            dawgora = import ./home.nix;
-        };
+      extraSpecialArgs = {inherit inputs; };
+      users = {
+        dawgora = import ./home.nix;
+      };
     };
 
     nixpkgs.config.allowUnfreePredicate = pkg:
-	builtins.elem (lib.getName pkg) [
-		"nvidia-x11"
-		"nvidia-settings"
-		"nvidia-persistenced"
-		"discord"
-		"spotify"
-		"obsidian"
-		"davinci-resolve"
-		"obs-studio"
-		"steam"
-		"plexmediaserver"
-		"vscode"
-	];
-    
+    builtins.elem (lib.getName pkg) [
+      "nvidia-x11"
+      "nvidia-settings"
+      "nvidia-persistenced"
+      "discord"
+      "spotify"
+      "obsidian"
+      "davinci-resolve"
+      "obs-studio"
+      "steam"
+      "plexmediaserver"
+      "vscode"
+    ];
+
 # bootloader settings
-    boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub = {
-      enable = true;
-      devices = [ "nodev" ];
-      efiSupport = true;
-      useOSProber = true;
-    };
+boot.loader = {
+  efi.canTouchEfiVariables = true;
+  grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiSupport = true;
+    useOSProber = true;
   };
+};
 
 # Experimental
 
@@ -56,8 +56,8 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 services.resolved.enable = true;
 
 # video settings
-  hardware.graphics = {
-    enable = true;
+hardware.graphics = {
+  enable = true;
     #driSupport = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
@@ -77,47 +77,47 @@ services.resolved.enable = true;
   };
 
 # razer
-  hardware.openrazer.enable = true;
+hardware.openrazer.enable = true;
 
 # wayland and hyperland
-  programs.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-  };
+programs.hyprland = {
+  enable = true;
+  xwayland.enable = true;
+};
 
-  programs.steam.enable = true;
+programs.steam.enable = true;
 
-  services.blueman.enable = true;
-  hardware.bluetooth.enable = true;
+services.blueman.enable = true;
+hardware.bluetooth.enable = true;
 
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+xdg.portal.enable = true;
+xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
+security.rtkit.enable = true;
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  jack.enable = true;
+};
 
 
 # virt manager
-  virtualisation.libvirtd.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-  programs.virt-manager.enable = true;
+virtualisation.libvirtd.enable = true;
+virtualisation.spiceUSBRedirection.enable = true;
+programs.virt-manager.enable = true;
 
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true;
+services.qemuGuest.enable = true;
+services.spice-vdagentd.enable = true;
 
 # docker
-  virtualisation.docker.enable = true;
+virtualisation.docker.enable = true;
 # shell settings
 
-  users.defaultUserShell = pkgs.zsh;
+users.defaultUserShell = pkgs.zsh;
 
-  networking.hostName = "dawgora"; # Define your hostname.
+networking.hostName = "dawgora"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
@@ -138,20 +138,20 @@ services.resolved.enable = true;
   # };
 
   environment.pathsToLink = [ "/libexec" ];
-  
+
   services.xserver = {
     enable = true;
 
     videoDrivers = ["nvidia"];
     xkb.layout = "lv";
   };
-  
-    services.displayManager = {
-      defaultSession = "hyprland";
-      sddm.enable = true;
-      sddm.theme = "${import ./modules/sddm/sddm-theme.nix { inherit pkgs; }}";
-      sddm.autoNumlock = true;
-    };
+
+  services.displayManager = {
+    defaultSession = "hyprland";
+    sddm.enable = true;
+    sddm.theme = "${import ./modules/sddm/sddm-theme.nix { inherit pkgs; }}";
+    sddm.autoNumlock = true;
+  };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -217,8 +217,8 @@ services.resolved.enable = true;
   ];
 
   fonts.packages = with pkgs; [
-   font-awesome
-   inter
+    font-awesome
+    inter
   ];
 
  #environment.extraInit = ''
@@ -228,14 +228,14 @@ services.resolved.enable = true;
  environment.localBinInPath = true;
 
  environment.sessionVariables = {
-  WLR_NO_HARDWARE_CURSORS = "1";
-  NIXOS_OZONE_WL = "1";
-  MOZ_DISABLE_RDD_SANDBOX = "1";
-  LIBVA_DRIVER_NAME = "nvidia";
-  GBM_BACKEND = "nvidia-drm";
-  __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-  NVD_BACKEND = "direct";
-  EGL_PLATFORM = "wayland";
+   WLR_NO_HARDWARE_CURSORS = "1";
+   NIXOS_OZONE_WL = "1";
+   MOZ_DISABLE_RDD_SANDBOX = "1";
+   LIBVA_DRIVER_NAME = "nvidia";
+   GBM_BACKEND = "nvidia-drm";
+   __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+   NVD_BACKEND = "direct";
+   EGL_PLATFORM = "wayland";
  };
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -264,9 +264,9 @@ services.resolved.enable = true;
     enableTCPIP = true;
     settings.port = 5432;
     authentication = lib.mkForce ''
-      local all all              trust
-      host  all all 127.0.0.1/32 trust
-      host  all all ::1/128      trust
+    local all all              trust
+    host  all all 127.0.0.1/32 trust
+    host  all all ::1/128      trust
     '';
   };
 
@@ -309,12 +309,12 @@ services.resolved.enable = true;
 
   nixpkgs.overlays = [
     (self: super: { 
-        waybar = super.waybar.overrideAttrs (oldAttrs: {
-          mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-        });
-        flameshot = super.flameshot.overrideAttrs (oldAttrs: {
-         enableWlrSupport = true;
-        });
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      });
+      flameshot = super.flameshot.overrideAttrs (oldAttrs: {
+        enableWlrSupport = true;
+      });
     })	
   ];
 
--- nixos/nixos/home.nix
+++ nixos/nixos/home.nix
@@ -1,14 +1,14 @@
 { config, pkgs, lib, ... }:
 
 let
-   customNeovim = import ./modules/nvim/nvim.nix;
-   elixir = import ./modules/elixir/flake.nix;
+  customNeovim = import ./modules/nvim/nvim.nix;
+  elixir = import ./modules/elixir/flake.nix;
 in
-{
+  {
 
-  imports = [
-    ./modules/waybar
-  ];
+    imports = [
+      ./modules/waybar
+    ];
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
@@ -27,97 +27,97 @@ in
   gtk = {
     enable = true;
     iconTheme = {
-    name = "WhiteSur-dark";
-    package = pkgs.whitesur-icon-theme;
-  };
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-icon-theme;
+    };
 
-  theme = {
-    name = "WhiteSur-Dark-solid";
-    package = pkgs.whitesur-gtk-theme;
-  };
+    theme = {
+      name = "WhiteSur-Dark-solid";
+      package = pkgs.whitesur-gtk-theme;
+    };
 
-  cursorTheme = {
-    name = "capitaine-cursors";
-    package = pkgs.capitaine-cursors;
-    size = 24;
+    cursorTheme = {
+      name = "capitaine-cursors";
+      package = pkgs.capitaine-cursors;
+      size = 24;
+    };
   };
-};
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-      pkgs.firefox
-      pkgs.librewolf
-      pkgs.ungoogled-chromium
-      pkgs.vscode
-      pkgs.p7zip
-      pkgs.udisks
-      pkgs.libreoffice-qt
-      pkgs.libpqxx
-      pkgs.tree
-      pkgs.vim
-      pkgs.arandr
-      pkgs.alacritty
-      pkgs.discord
-      pkgs.vesktop
-      pkgs.razergenie
-      pkgs.spotify
-      pkgs.telegram-desktop
-      pkgs.tailscale
-      pkgs.alarm-clock-applet
-      pkgs.tmux
-      pkgs.flameshot
-      pkgs.jq
-      pkgs.gettext
-      pkgs.htop
-      pkgs.btop
-      pkgs.dconf
-      pkgs.mpv
-      pkgs.xss-lock
-      pkgs.prismlauncher
-      pkgs.xfce.thunar
-      pkgs.ranger
-      pkgs.feh
-      pkgs.bitwarden
-      pkgs.keepass
-      pkgs.xfce.mousepad
-      pkgs.yubikey-personalization
-      pkgs.yubioath-flutter
-      pkgs.obsidian
-      pkgs.tigervnc
-      pkgs.neofetch
-      pkgs.signal-desktop
-      pkgs.davinci-resolve
-      pkgs.obs-studio
-      pkgs.qbittorrent
-      pkgs.vlc
-      pkgs.dbeaver-bin
-      pkgs.git
-      pkgs.unzip
-      pkgs.pamixer
-      pkgs.ncurses
-      pkgs.libssh
-      pkgs.openssl
-      pkgs.libssh
-      pkgs.icu
-      pkgs.tig
-      pkgs.postgresql
-      pkgs.docker
-      pkgs.inotify-tools
-      pkgs.nodejs_22
-      pkgs.networkmanagerapplet
-      pkgs.wireguard-tools
-      pkgs.jellyfin-ffmpeg
-      pkgs.yt-dlp
-      pkgs.bashInteractiveFHS
-      pkgs.plex
-      pkgs.plexRaw
-      pkgs.darktable
-      pkgs.python3
-      pkgs.bazecor
-      pkgs.hyprshot
-      pkgs.pavucontrol
-      pkgs.playerctl
+    pkgs.firefox
+    pkgs.librewolf
+    pkgs.ungoogled-chromium
+    pkgs.vscode
+    pkgs.p7zip
+    pkgs.udisks
+    pkgs.libreoffice-qt
+    pkgs.libpqxx
+    pkgs.tree
+    pkgs.vim
+    pkgs.arandr
+    pkgs.alacritty
+    pkgs.discord
+    pkgs.vesktop
+    pkgs.razergenie
+    pkgs.spotify
+    pkgs.telegram-desktop
+    pkgs.tailscale
+    pkgs.alarm-clock-applet
+    pkgs.tmux
+    pkgs.flameshot
+    pkgs.jq
+    pkgs.gettext
+    pkgs.htop
+    pkgs.btop
+    pkgs.dconf
+    pkgs.mpv
+    pkgs.xss-lock
+    pkgs.prismlauncher
+    pkgs.xfce.thunar
+    pkgs.ranger
+    pkgs.feh
+    pkgs.bitwarden
+    pkgs.keepass
+    pkgs.xfce.mousepad
+    pkgs.yubikey-personalization
+    pkgs.yubioath-flutter
+    pkgs.obsidian
+    pkgs.tigervnc
+    pkgs.neofetch
+    pkgs.signal-desktop
+    pkgs.davinci-resolve
+    pkgs.obs-studio
+    pkgs.qbittorrent
+    pkgs.vlc
+    pkgs.dbeaver-bin
+    pkgs.git
+    pkgs.unzip
+    pkgs.pamixer
+    pkgs.ncurses
+    pkgs.libssh
+    pkgs.openssl
+    pkgs.libssh
+    pkgs.icu
+    pkgs.tig
+    pkgs.postgresql
+    pkgs.docker
+    pkgs.inotify-tools
+    pkgs.nodejs_22
+    pkgs.networkmanagerapplet
+    pkgs.wireguard-tools
+    pkgs.jellyfin-ffmpeg
+    pkgs.yt-dlp
+    pkgs.bashInteractiveFHS
+    pkgs.plex
+    pkgs.plexRaw
+    pkgs.darktable
+    pkgs.python3
+    pkgs.bazecor
+    pkgs.hyprshot
+    pkgs.pavucontrol
+    pkgs.playerctl
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -126,27 +126,27 @@ in
   };
 
   programs.zsh = {
-  enable = true;
-  initExtra = ''
-        . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
-        . "${pkgs.asdf-vm}/share/asdf-vm/completions/asdf.bash"
-  '';
+    enable = true;
+    initExtra = ''
+    . "${pkgs.asdf-vm}/share/asdf-vm/asdf.sh"
+    . "${pkgs.asdf-vm}/share/asdf-vm/completions/asdf.bash"
+    '';
   };
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "nvidia-x11"
-	"nvidia-settings"
-	"nvidia-persistenced"
-	"discord"
-	"spotify"
-	"obsidian"
-	"davinci-resolve"
-	"obs-studio"
-	"steam"
+    "nvidia-settings"
+    "nvidia-persistenced"
+    "discord"
+    "spotify"
+    "obsidian"
+    "davinci-resolve"
+    "obs-studio"
+    "steam"
     "steam-unwrapped"
-	"plexmediaserver"
-	"vscode"
-      ];
+    "plexmediaserver"
+    "vscode"
+  ];
 
   programs.neovim = customNeovim pkgs;
   # Home Manager can also manage your environment variables through

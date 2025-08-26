d20f3fe41246a161064b0c258dbbf2e5bbe46979
Dawgora <Dawgora@protonmail.com>
2025-08-27 02:14:21 +0300

    added work related config

---
+++ nixos/nixos/hosts/work/configuration.nix
@@ -0,0 +1,287 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ inputs, config, lib, pkgs, callPackage, ... }:
+
+let
+  #unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+  {
+    imports =
+      [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../modules/nginx
+      ../../modules/zsh
+      ../../modules/steam
+      inputs.home-manager.nixosModules.home-manager
+    ];
+
+    home-manager = {
+      extraSpecialArgs = {inherit inputs; };
+      users = {
+        dawgora = import ./home.nix;
+      };
+    };
+
+    nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+        "discord"
+        "spotify"
+        "obsidian"
+    ];
+
+# bootloader settings
+boot.loader = {
+  efi.canTouchEfiVariables = true;
+  grub = {
+    enable = true;
+    devices = [ "nodev" ];
+    efiSupport = true;
+    useOSProber = true;
+  };
+};
+
+# Experimental
+
+nix.settings.experimental-features = ["nix-command" "flakes"];
+
+services.resolved.enable = true;
+
+# video settings
+hardware.graphics = {
+  enable = true;
+    #driSupport = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      vdpauinfo # sudo vainfo
+      libva-utils # sudo vainfo
+    ];
+  };
+
+# wayland and hyperland
+programs.hyprland = {
+  enable = true;
+  xwayland.enable = true;
+};
+
+
+services.blueman.enable = true;
+hardware.bluetooth.enable = true;
+
+xdg.portal.enable = true;
+xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+security.rtkit.enable = true;
+services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  jack.enable = true;
+};
+
+
+# virt manager
+virtualisation = {
+  libvirtd = {
+    enable = true;
+    qemu = {
+      swtpm.enable = true;
+      ovmf.enable = true;
+      ovmf.packages = [pkgs.OVMFFull.fd];
+    };
+  };
+  spiceUSBRedirection.enable = true;
+};
+
+#programs.virt-manager.enable = true;
+
+services.qemuGuest.enable = true;
+services.spice-vdagentd.enable = true;
+
+# docker
+virtualisation.docker.enable = true;
+# shell settings
+
+users.defaultUserShell = pkgs.zsh;
+
+networking.hostName = "dawgora"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  networking.firewall.trustedInterfaces = [ "virbr0" ];
+
+  # Set your time zone.
+  time.timeZone = "Europe/Riga";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  environment.pathsToLink = [ "/libexec" ];
+
+  services.xserver = {
+    enable = true;
+
+    xkb.layout = "lv";
+  };
+
+  services.displayManager = {
+    defaultSession = "hyprland";
+    sddm.enable = true;
+    sddm.theme = "${import ../../modules/sddm/sddm-theme.nix { inherit pkgs; }}";
+    sddm.autoNumlock = true;
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+
+  nix.settings.allowed-users = [ "@wheel" ];
+
+  # Enable touchpad support (enabled default in most delibvirtdsktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dawgora = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" "docker" "vboxsf" "libvirtd"];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    vdpauinfo # sudo vainfo
+    libva-utils # sudo vainfo
+    home-manager
+    tmux
+    htop
+    nix-prefetch-git
+    libsForQt5.qt5.qtquickcontrols2
+    libsForQt5.qt5.qtgraphicaleffects
+    gparted
+    wlogout
+    git
+    postgresql_jdbc
+    openssl
+    waybar
+    mako
+    libnotify
+    rofi-wayland
+    swww
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts.githubRelease
+    dina-font
+    font-awesome
+    proggyfonts
+    swaylock
+    swayidle
+    sway-audio-idle-inhibit
+    grim
+    slurp
+    wl-clipboard
+    alsa-utils
+    rose-pine-hyprcursor
+    virt-manager
+    virt-viewer
+    spice
+    spice-protocol
+    spice-gtk
+    adwaita-icon-theme
+  ];
+
+  fonts.packages = with pkgs; [
+    font-awesome
+    inter
+  ];
+
+ environment.localBinInPath = true;
+
+ environment.sessionVariables = {
+   WLR_NO_HARDWARE_CURSORS = "1";
+   NIXOS_OZONE_WL = "1";
+   MOZ_DISABLE_RDD_SANDBOX = "1";
+  NVD_BACKEND = "direct";
+  EGL_PLATFORM = "wayland";
+};
+
+services.desktopManager.gnome.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # all services
+  services.envfs.enable = true;
+
+  # TIME
+  time.hardwareClockInLocalTime = true;
+
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    enableTCPIP = true;
+    settings.port = 5432;
+    authentication = lib.mkForce ''
+      local all all              trust
+      host  all all 127.0.0.1/32 trust
+      host  all all ::1/128      trust
+    '';
+  };
+
+
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nixpkgs.overlays = [
+    (self: super: {
+      waybar = super.waybar.overrideAttrs (oldAttrs: {
+        mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      });
+      flameshot = super.flameshot.overrideAttrs (oldAttrs: {
+        enableWlrSupport = true;
+      });
+    })
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+}
---
+++ nixos/nixos/hosts/work/home.nix
@@ -0,0 +1,162 @@
+{ config, pkgs, lib, ... }:
+
+let
+  customNeovim = import ../../modules/nvim;
+  elixir = ../../modules/elixir;
+in
+  {
+
+    imports = [
+      ../../modules/waybar
+      ../../modules/hyprland
+    ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "dawgora";
+  home.homeDirectory = "/home/dawgora";
+
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  home.pointerCursor = {
+    name = "capitaine-cursors";
+    package = pkgs.capitaine-cursors;
+    size = 24;
+  };
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-icon-theme;
+    };
+
+    theme = {
+      name = "WhiteSur-Dark-solid";
+      package = pkgs.whitesur-gtk-theme;
+    };
+
+    cursorTheme = {
+      name = "capitaine-cursors";
+      package = pkgs.capitaine-cursors;
+      size = 24;
+    };
+  };
+
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    firefox
+    librewolf
+    p7zip
+    udisks
+    libreoffice-qt
+    libpqxx
+    tree
+    vim
+    arandr
+    alacritty
+    discord
+    razergenie
+    spotify
+    telegram-desktop
+    tailscale
+    alarm-clock-applet
+    tmux
+    flameshot
+    jq
+    gettext
+    htop
+    btop
+    dconf
+    mpv
+    xss-lock
+    prismlauncher
+    xfce.thunar
+    ranger
+    feh
+    bitwarden
+    keepass
+    xfce.mousepad
+    yubikey-personalization
+    yubioath-flutter
+    obsidian
+    tigervnc
+    neofetch
+    signal-desktop
+    vlc
+    dbeaver-bin
+    git
+    unzip
+    pamixer
+    ncurses
+    libssh
+    openssl
+    libssh
+    icu
+    tig
+    postgresql
+    docker
+    inotify-tools
+    nodejs_22
+    networkmanagerapplet
+    wireguard-tools
+    yt-dlp
+    bashInteractiveFHS
+    python3
+    hyprshot
+    pavucontrol
+    playerctl
+    zed-editor
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+#  # plain files is through 'home.file'.
+home.file = {
+};
+
+programs.zsh = {
+  enable = true;
+  initExtra = ''
+  '';
+};
+
+
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "discord"
+    "spotify"
+    "obsidian"
+  ];
+
+  programs.neovim = customNeovim pkgs;
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/dawgora/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    TERMINAL = "alacritty";
+    GTK_THEME = "WhiteSur-Dark-solid";
+    NIXOS_HOME_CONFIG = "/home/dawgora/.config";
+  };
+
+  home.shellAliases = {
+    "elixir-devel"="nix-shell ${elixir} --command zsh";
+    "rebuild-local-nixos" = "sudo nixos-rebuild switch --flake ~/flakes/nixos/#workPC";
+    "update-nixos-channel" = "sudo nix-channel --update";
+    "delete-nixos-garbage" = "nix-collect-garbage --delete-old";
+    "update-home-flake" = "sudo nix flake update --flake ~/flakes/nixos";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}

1abe3c8732aae7f83edbe53f5142c546c37f9b49
Dawgora <dawgora@protonmail.com>
2025-10-15 00:00:08 +0300

    updated shikane config

--- nixos/nixos/modules/sway/default.nix
+++ nixos/nixos/modules/sway/default.nix
@@ -60,7 +60,8 @@ in
       #bar = {};
       focus.followMouse = true;
       startup = [
-        {command = "pkill kanshi; exec kanshi"; always = true;}
+        {command = "shikane";}
+        #{command = "pkill kanshi; exec kanshi"; always = true;}
         #{command = "systemctl --user restart kanshi"; always = true;}
         {command = "--no-startup-id /home/dawgora/.local/bin/start.sh";}
         {command = "--no-startup-id /home/dawgora/.local/bin/sleep.sh";}

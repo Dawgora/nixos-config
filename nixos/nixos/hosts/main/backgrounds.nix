710a32aee1b44a1f54835f1f361f59fd887cf3fe
Dawgora <dawgora@protonmail.com>
2026-09-25 01:46:01 +0300

    wallpaper setup

--- nixos/nixos/hosts/main/backgrounds.nix
+++ nixos/nixos/hosts/main/backgrounds.nix
@@ -7,11 +7,11 @@
   home.packages = [
     (pkgs.writeShellScriptBin "set-backgrounds" ''
       ${pkgs.awww}/bin/awww img -o HDMI-A-2 \
-        ~/.local/share/backgrounds/side-screens.jpg --resize fit
+        ~/.local/share/backgrounds/side-screens.png --resize fit
       ${pkgs.awww}/bin/awww img -o DP-3 \
         ~/.local/share/backgrounds/middle-screen-2.jpg --resize fit
       ${pkgs.awww}/bin/awww img -o DP-4 \
-        ~/.local/share/backgrounds/side-screens.jpg --resize fit
+        ~/.local/share/backgrounds/side-screens.png --resize fit
     '')
   ];
 }
